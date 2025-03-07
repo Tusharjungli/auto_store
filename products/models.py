0d0ade822b29b0cf7e78b99903c34ffdf594c3a1
Tusharjungli <trpanchaltr@gmail.com>
2025-03-07 11:50:47 +0530

    v1.2 - Added search, filering and sorting to products

--- products/models.py
+++ products/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('engine', 'Engine Parts'),
+        ('brake', 'Brake System'),
+        ('lights', 'Lights & Indicators'),
+        ('battery', 'Battery & Electrical'),
+        ('accessory', 'Accessories'),
+    ]
+
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to="product_images/")
+    image = models.ImageField(upload_to='product_images/')
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='accessory')
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -2,5 +2,25 @@ from django.shortcuts import render
 from .models import Product
 
 def product_list(request):
-    products = Product.objects.all()  # Get all products
-    return render(request, "products/product_list.html", {"products": products})
+    query = request.GET.get('q')
+    category = request.GET.get('category')
+    sort_by = request.GET.get('sort')
+
+    products = Product.objects.all()
+
+    if query:
+        products = products.filter(name__icontains=query)
+
+    if category:
+        products = products.filter(category=category)
+
+    if sort_by == 'price_low':
+        products = products.order_by('price')
+    elif sort_by == 'price_high':
+        products = products.order_by('-price')
+
+    return render(request, 'products/product_list.html', {
+        'products': products,
+        'selected_category': category,
+        'selected_sort': sort_by,
+    })
